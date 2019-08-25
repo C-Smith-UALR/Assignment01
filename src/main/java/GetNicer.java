eeb9a8fa4ae4871913328fa37eefdde6d2fcfbcf
C-Smith-UALR <clarklander1983@gmail.com>
2019-08-25 19:08:21 +0000

    first commit.  trying to link github and codenvy

---
+++ src/main/java/GetNicer.java
@@ -0,0 +1,10 @@
+
+public class GetNicer {
+    
+    public static void main(String[] args){
+        System.out.println("Get Nicer than I ever Could");
+    }
+    
+}
+
+//these are just comments
